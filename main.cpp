9c7b43cb58cc3acf21bb6e254b92a47cdd4d1259
CaiNot <303487454@qq.com>
2018-11-01 20:24:20 +0800

    完成了分治法，并且修改了文件输入输出使其满足要求

--- main.cpp
+++ main.cpp
@@ -21,27 +21,29 @@ int distance(int *point_1, int *point_2) {
 bool cmpX(int *p, int *q) {
     if (p[0] == q[0]) {
         return p[1] < q[1];
-    }
-    else
+    } else
         return p[0] < q[0];
 }
 
 bool cmpY(int *p, int *q) {
     if (p[1] == q[1]) {
         return p[0] < q[0];
-    }
-    else
+    } else
         return p[1] < q[1];
 }
 
 class Solution {
 private:
     int pointsNum; // 总共的点数
+    vector<vector<int *>> groups;
     vector<int *> points;
     vector<int *> pointsX; // 以X为基准 升序
     vector<int *> pointsY; // 以Y为基准 升序
+    vector<int *> pointsT;
     int minDis; // 保存最短距离
     int point_1[2], point_2[2]; // 保存最近点对
+
+    vector<int> result_XY;
 
 public:
     Solution() : pointsNum(0), points(0) {
@@ -52,7 +54,49 @@ public:
         this->point_2[1] = -1;
     }
 
+//    bool readFromFile(string filePath) {
+//        int x, y;
+//        string line;
+//        ifstream infile(filePath);
+//        stringstream ss;
+//
+//        if (!infile.is_open()) {
+//            cout << "File Open Error" << endl;
+//            return false;
+//        }
+//
+//        int pos = 0;
+//        int *posTemp = 0;
+//        while (!infile.eof()) {
+//            getline(infile, line);
+//            ss.clear();
+//            ss.str(line);
+//            ss >> x >> y;
+//            if (ss.fail()) {
+//                continue;
+//            }
+//            posTemp = new int[2];
+//            posTemp[0] = x;
+//            posTemp[1] = y;
+//            if (y == 25918) {
+//                cout << "x: " << x << " y: " << y << endl;
+//
+//            }
+//            this->points.push_back(posTemp);
+//
+//            pos++;
+//
+//            //            cout << "x: " << x << " y: " << y << endl;
+//        }
+//        this->pointsNum = pos;
+//        cout << "the num of points is " << pos << endl;
+//
+//        infile.close();
+//        return true;
+//    }
+
     bool readFromFile(string filePath) {
+
         int x, y;
         string line;
         ifstream infile(filePath);
@@ -65,29 +109,45 @@ public:
 
         int pos = 0;
         int *posTemp = 0;
-        while (!infile.eof()) {
+
+        int groupNum = 0;
+        getline(infile, line);
+        ss.clear();
+        ss.str(line);
+        ss >> groupNum;
+        if (ss.fail()) {
+            cout << "File Read Error" << endl;
+            return false;
+        }
+        int oneTestNum = 0;
+        for (int i = 0; i < groupNum; i++) {
             getline(infile, line);
             ss.clear();
             ss.str(line);
-            ss >> x >> y;
+            ss >> oneTestNum;
             if (ss.fail()) {
-                continue;
+                cout << "File Read Error" << endl;
+                return false;
             }
-            posTemp = new int[2];
-            posTemp[0] = x;
-            posTemp[1] = y;
-            if (y == 25918) {
-                cout << "x: " << x << " y: " << y << endl;
-
+            pos = 0;
+            for (int j = 0; j < oneTestNum; j++) {
+                getline(infile, line);
+                ss.clear();
+                ss.str(line);
+                ss >> x >> y;
+                if (ss.fail()) {
+                    continue;
+                }
+                posTemp = new int[2];
+                posTemp[0] = x;
+                posTemp[1] = y;
+                this->points.push_back(posTemp);
+                pos++;
             }
-            this->points.push_back(posTemp);
-
-            pos++;
-
-            //            cout << "x: " << x << " y: " << y << endl;
+            this->groups.push_back(this->points);
+            this->points.clear();
         }
-        this->pointsNum = pos;
-        cout << "the num of points is " << pos << endl;
+
 
         infile.close();
         return true;
@@ -115,8 +175,14 @@ public:
         this->pointsX.assign(this->points.begin(), this->points.end());
     }
 
+    void sortByY() {
+        sort(this->points.begin(), this->points.end(), cmpY);
+        this->pointsY.assign(this->points.begin(), this->points.end());
+    }
+
     void sortByY(vector<int *> &my_points, int start, int end) {
-        sort(my_points.begin() + start, my_points.begin() + end, cmpY);
+        this->pointsT.assign(my_points.begin() + start, my_points.begin() + end);
+        sort(this->pointsT.begin(), this->pointsT.end(), cmpY);
         //        this->pointsY.assign(this->points.begin(), this->points.end());
     }
 
@@ -130,9 +196,21 @@ public:
     }
 
     void solveByDivider() {
-        this->sortByX();
-        //        this->sortByY();
-        minDisArea(0, this->pointsNum - 1);
+        ofstream saveData("out.dat");
+        for (int i = 0; i < this->groups.size(); i++) {
+            this->points = this->groups[i];
+            this->pointsNum = this->points.size();
+            this->minDis = INT_MAX;
+
+            this->sortByX();
+            //        this->sortByY();
+            minDisArea(0, this->pointsNum - 1);
+            this->printResult();
+            for (int j = 0; j < this->result_XY.size(); j++) {
+                saveData << this->result_XY[j] << "\t";
+            }
+            saveData << endl;
+        }
     }
 
 
@@ -157,21 +235,28 @@ public:
 
             int minDisM = minDisMid(startMid, endMid);
             this->minDis = min(this->minDis, minDisM);;
-        }
-        else {
+        } else {
             int disTemp = 0;
             for (int i = start; i <= end; i++) {
                 for (int j = i + 1; j <= end; j++) {
-                    if (this->pointsX[i][0] == 27933 && this->pointsX[i][1] == 19502) {
-                        disTemp = 0;
-                    }
+
                     disTemp = distance(this->pointsX[i], this->pointsX[j]);
-                    if (this->minDis > disTemp) {
+                    if (this->minDis >= disTemp) {
+                        if (this->minDis != disTemp) {
+                            this->result_XY.clear();
+                        }else if (this->isSame(this->pointsX[i][0], this->pointsX[i][1], pointsX[j][0], pointsX[j][1]))
+                            continue;
                         this->minDis = disTemp;
+
                         this->point_1[0] = this->pointsX[i][0];
                         this->point_1[1] = this->pointsX[i][1];
                         this->point_2[0] = this->pointsX[j][0];
                         this->point_2[1] = this->pointsX[j][1];
+
+                        this->result_XY.push_back(this->pointsX[i][0]);
+                        this->result_XY.push_back(this->pointsX[i][1]);
+                        this->result_XY.push_back(this->pointsX[j][0]);
+                        this->result_XY.push_back(this->pointsX[j][1]);
                     }
                 }
             }
@@ -180,29 +265,39 @@ public:
     }
 
     int minDisMid(int start, int end) {
-        int disTemp = 0;
+        int disTemp = 0, len = end - start;
         this->sortByY(this->pointsX, start, end + 1);
-        for (int i = start; i <= end; i++) {
-            for (int j = i + 1; j <= end; j++) {
-                if (this->pointsX[i][0] == 27933 && this->pointsX[i][1] == 19502) {
-                    disTemp = 0;
-                }
-                if (abs(this->pointsX[i][1] - this->pointsX[j][1]) > this->minDis) {
+        for (int i = 0; i <= len; i++) {
+            for (int j = i + 1; j <= len && j <= 7 + i; j++) {
+
+                if (abs(this->pointsT[i][1] - this->pointsT[j][1]) > this->minDis) {
                     break;
                 }
-
-                disTemp = distance(this->pointsX[i], this->pointsX[j]);
-                if (this->minDis > disTemp) {
+                disTemp = distance(this->pointsT[i], this->pointsT[j]);
+                if (this->minDis >= disTemp) {
+                    if (this->minDis != disTemp) {
+                        this->result_XY.clear();
+                    } else if (this->isSame(this->pointsT[i][0], this->pointsT[i][1], pointsT[j][0], pointsT[j][1]))
+                        continue;
                     this->minDis = disTemp;
-                    this->point_1[0] = this->pointsX[i][0];
-                    this->point_1[1] = this->pointsX[i][1];
-                    this->point_2[0] = this->pointsX[j][0];
-                    this->point_2[1] = this->pointsX[j][1];
+                    this->point_1[0] = this->pointsT[i][0];
+                    this->point_1[1] = this->pointsT[i][1];
+                    this->point_2[0] = this->pointsT[j][0];
+                    this->point_2[1] = this->pointsT[j][1];
+
+                    this->result_XY.push_back(this->pointsT[i][0]);
+                    this->result_XY.push_back(this->pointsT[i][1]);
+                    this->result_XY.push_back(this->pointsT[j][0]);
+                    this->result_XY.push_back(this->pointsT[j][1]);
                 }
-                //                this->minDis = min(this->minDis, distance(this->pointsX[i], this->pointsX[j]));
             }
         }
         return this->minDis;
+    }
+
+    bool isSame(int x_1, int y_1, int x_2, int y_2) {
+        return this->point_1[0] == x_1 && this->point_1[1] == y_1
+               && this->point_2[0] == x_2 && this->point_2[1] == y_2;
     }
 
     void printResult() {
@@ -220,8 +315,11 @@ public:
     }
 
     ~Solution() {
-        for (int i = 0; i < this->points.size(); i++) {
-            delete[] this->points[i];
+        for (int j = 0; j < this->groups.size(); j++) {
+            this->points = this->groups[j];
+            for (int i = 0; i < this->points.size(); i++) {
+                delete[] this->points[i];
+            }
         }
     }
 };
@@ -230,7 +328,7 @@ class Data {
 public:
     void randomNumberToFile(string filePath, int num, int range) {
         ofstream outfile(filePath);
-        srand((unsigned int)time(0));
+        srand((unsigned int) time(0));
         for (int i = 0; i < num; i++) {
             outfile << rand() % range << " " << rand() % range << endl;
         }
@@ -241,10 +339,10 @@ public:
 
 int main() {
     //    std::cout << "Hello, World!" << std::endl;
-//        Data d;
-//        d.randomNumberToFile("D://points.txt", 15000, 300000);
+//    Data d;
+//    d.randomNumberToFile("D://points.txt", 5000, 600000);
     Solution s;
-    s.readFromFile("D://points.txt");
+    s.readFromFile("D://Data.txt");
 
 
     DWORD start, end;
@@ -255,14 +353,14 @@ int main() {
     end = GetTickCount();
     totalTime = end - start;
     cout << "分治法的时间: " << totalTime << endl;
-    s.printResult();
+//    s.printResult();
 
     start = GetTickCount();
-    s.solveByCommon();
+//    s.solveByCommon();
     end = GetTickCount();
     totalTime = end - start;
     cout << "搜索法的时间: " << totalTime << endl;
 
-    s.printResult();
+//    s.printResult();
     return 0;
 }
